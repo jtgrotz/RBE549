95a1cce487c7b8b6d2e727c14740a80af1b13e55
jtgrotz <jordantaylorg007@gmail.com>
2023-08-29 18:37:38 -0400

    merged functions and added comments

--- Lab1/photobooth.py
+++ Lab1/photobooth.py
@@ -1,10 +1,33 @@
 import cv2 as cv
 import numpy as np
-from zoom_in import zoom_in
 from datetime import datetime
 
+#function to do nothing as callback for trackbar functions
 def nothing(x):
     pass
+
+#function for zooming in on the center of the image
+def zoom_in(image, zoom_level):
+    #saves original image size and zoomed image size
+    s = image.shape
+    h = s[0]
+    w = s[1]
+    zoomed_h = zoom_level * h
+    zoomed_w = zoom_level * w
+    center_width = int(zoomed_w/2)
+    center_height = int(zoomed_h/2)
+
+    #creates resized image of new dimensions with linear interpolation
+    image = cv.resize(image,(0,0), fx=zoom_level, fy=zoom_level, interpolation=cv.INTER_LINEAR)
+
+    #only displays the resolution of the original image with
+    # a center focal point.
+    image = image[int(center_height - (h/2)) : int(center_height + (h/2)),
+          int(center_width - (w/2)): int(center_width + (w/2)),
+          :]
+
+    return image
+
 
 #global variables
 isrecording = False
@@ -25,6 +48,7 @@ fourcc = cv.VideoWriter_fourcc(*'XVID')
 
 
 while vid.isOpened():
+    #reads intial frame from camera and then determines zoom level and applies it
     ret, frame = vid.read()
     z = (cv.getTrackbarPos('Zoom','photobooth')/10)+1
     frame = zoom_in(frame,z)
@@ -58,7 +82,7 @@ while vid.isOpened():
     cv.putText(frame, 'Capture',(int(0.55*vid_width), int(0.84*vid_height)), font, 1, (255,255,255), 1, cv.LINE_AA)
     cv.putText(frame, 'V',(int(0.385*vid_width),int(0.92*vid_height)),font, 2, (255,255,255), 2, cv.LINE_AA)
     cv.putText(frame, 'C',(int(0.58*vid_width),int(0.92*vid_height)),font, 2, (255,255,255), 2, cv.LINE_AA)
-
+    cv.putText(frame, 'Press',(int(0.2*vid_width),int(0.92*vid_height)),font, 2, (255,255,255), 2, cv.LINE_AA)
     cv.imshow('photobooth', zoom_in(frame,1))
 
 
@@ -77,14 +101,14 @@ while vid.isOpened():
             #if not recornding then start recording
             isrecording = True
             file_name = "video_" + file_time_string + ".avi"
-            out = cv.VideoWriter(file_name,fourcc, 30.0, (640,480))
+            out = cv.VideoWriter(file_name,fourcc, 40.0, (640,480))
 
         elif isrecording:
             # stop and save recording
             isrecording = False
             out.release()
 
-
+#ensure video and recording are ended properly.
 vid.release()
 if out is not None:
     out.release()
--- Lab1/zoom_in.py
+++ Lab1/zoom_in.py
@@ -2,15 +2,17 @@ import cv2 as cv
 
 def zoom_in(image, zoom_level):
     s = image.shape
-    zoomed_h = zoom_level * s[0]
-    zoomed_w = zoom_level * s[1]
+    h = s[0]
+    w = s[1]
+    zoomed_h = zoom_level * h
+    zoomed_w = zoom_level * w
     center_width = int(zoomed_w/2)
     center_height = int(zoomed_h/2)
 
     image = cv.resize(image,(0,0), fx=zoom_level, fy=zoom_level, interpolation=cv.INTER_LINEAR)
 
-    image = image[int(round(center_height - zoomed_h / zoom_level * .5)): int(round(center_height + zoomed_h / zoom_level * .5)),
-          int(round(center_width - zoomed_w / zoom_level * .5)): int(round(center_width + zoomed_w / zoom_level * .5)),
+    image = image[int(round(center_height - (h/2))) : int(round(center_height + (h/2))),
+          int(round(center_width - (w/2))): int(round(center_width + (w/2))),
           :]
 
     return image
