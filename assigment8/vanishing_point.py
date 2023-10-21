1b7dfaba703441139a21f8b4b60095c2061ecdcf
jtgrotz <jordantaylorg007@gmail.com>
2023-10-21 17:14:08 -0400

    finished assignment 8

--- assigment8/vanishing_point.py
+++ assigment8/vanishing_point.py
@@ -3,6 +3,8 @@ import numpy as np
 
 #import photo
 texas = cv.imread('texas.png')
+cv.imshow('img',texas)
+cv.waitKey(0)
 
 
 #apply canny edge detection to extract edges
@@ -36,9 +38,31 @@ cv.waitKey(0)
 
 
 
-#form least sqaures format
-np.linalg.lstsq()
-#solve using SVD
+#form least squares format
+num_lines = len(lines)
+#create a and b matrix shapes
+a = np.zeros((num_lines,2),np.float32)
+b = np.zeros((num_lines,1),np.float32)
+#populate with actual values
+for i in range(num_lines):
+    rho = lines[i][0][0]
+    theta = lines[i][0][1]
+    #b = [rho_1...rho_n]'
+    b[i] = rho
+    #a = [cos(theta_n) sin(theta_n); ....]
+    a[i][0] = np.cos(theta)
+    a[i][1] = np.sin(theta)
+
+#solve using least squares
+x, r, rank, sing = np.linalg.lstsq(a,b)
+print(x) #this is the solution of the least squares
 
 
 #visually mark the point on the image
+#convert to integer
+vanish_x = int(x[0][0])
+vanish_y = int(x[1][0])
+#draw circle
+cv.circle(texas,(vanish_x,vanish_y),12,(255,0,0),-1)
+cv.imshow('img',texas)
+cv.waitKey(0)
