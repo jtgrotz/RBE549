dc1360eae00594e8158f1059a2eb01f1eeca043c
jtgrotz <jordantaylorg007@gmail.com>
2023-10-27 18:36:44 -0400

    Finished Lab 8

--- lab8/lab8.py
+++ lab8/lab8.py
@@ -2,9 +2,17 @@ import numpy as np
 import cv2 as cv
 import glob
 
+#img_set = "original"
+img_set = "custom"
+
 #settings for checkerboard size
-pattern_x = 7
-pattern_y = 11
+if img_set == "original":
+    pattern_x = 7
+    pattern_y = 11
+elif img_set == "custom":
+    #settings for custom photos
+    pattern_x = 4
+    pattern_y = 4
 
 #set termination criteria
 criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
@@ -18,7 +26,11 @@ objpoints = []
 imgpoints = []
 
 #find image names
-images = glob.glob('calibration_data/*.jpg')
+if img_set == "original":
+    images = glob.glob('calibration_data/*.jpg')
+elif img_set == "custom":
+    #find image nammes for custom images
+    images = glob.glob('calibration_data2/*.png')
 
 for iname in images:
     img = cv.imread(iname)
@@ -50,12 +62,17 @@ print(dist)
 #print(rvecs)
 #print("translation vectors")
 #print(tvecs)
-np.save("calibration_matrix", mtx)
-np.save("distortion_coefficients", dist)
+np.save("calibration_matrix_"+img_set, mtx)
+np.save("distortion_coefficients_"+img_set, dist)
 #undistortion
 
-#reference image
-image = cv.imread('calibration_data/IMG_6528.jpg')
+if img_set == "original":
+    #reference image
+    image = cv.imread('calibration_data/IMG_6528.jpg')
+elif img_set == "custom":
+    #reference image for custom implementation
+    image = cv.imread('calibration_data2/IMG_6063.png')
+
 h,w = image.shape[:2]
 newcameramtx, roi = cv.getOptimalNewCameraMatrix(mtx,dist,(w,h),1,(w,h))
 
@@ -65,7 +82,7 @@ un_dist = cv.undistort(image,mtx,dist,None,newcameramtx)
 #crop the image based on distorition
 x, y, w, h, = roi
 un_dist = un_dist[y:y+h, x:x+w]
-#cv.imwrite('calibration_result.jpg',un_dist)
+cv.imwrite('calibration_result_'+img_set+'.jpg',un_dist)
 cv.imshow('distorted',image)
 cv.imshow('undistorted',un_dist)
 cv.waitKey(0)
