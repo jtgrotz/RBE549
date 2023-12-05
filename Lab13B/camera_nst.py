e42c4d98368ed53d2890906f230ea3f4a8a9c5e6
jtgrotz <jordantaylorg007@gmail.com>
2023-12-04 19:01:32 -0500

    finished lab13B

--- Lab13B/camera_nst.py
+++ Lab13B/camera_nst.py
@@ -1,5 +1,5 @@
 import os
-
+import cv2 as cv
 import keras
 import tensorflow as tf
 # Load compressed models from tensorflow_hub
@@ -46,6 +46,22 @@ def load_img(path_to_img):
   img = img[tf.newaxis, :]
   return img
 
+#function for making image the right dimensions for video stream
+def convert_image(img):
+    max_dim = 512
+    #img = tf.image.decode_image(img, channels=3)
+    img = tf.image.convert_image_dtype(img, tf.float32)
+
+    shape = tf.cast(tf.shape(img)[:-1], tf.float32)
+    long_dim = max(shape)
+    scale = max_dim / long_dim
+
+    new_shape = tf.cast(shape * scale, tf.int32)
+
+    img = tf.image.resize(img, new_shape)
+    img = img[tf.newaxis, :]
+    return img
+
 #load in content and style image
 content_path = tf.keras.utils.get_file('YellowLabradorLooking_new.jpg', 'https://storage.googleapis.com/download.tensorflow.org/example_images/YellowLabradorLooking_new.jpg')
 style_path = tf.keras.utils.get_file('kandinsky5.jpg','https://storage.googleapis.com/download.tensorflow.org/example_images/Vassily_Kandinsky%2C_1913_-_Composition_7.jpg')
@@ -59,15 +75,7 @@ def imshow(image, title=None):
   if title:
     plt.title(title)
 
-#display chosen images
-content_image = load_img(content_path)
-style_image = load_img(style_path)
 
-plt.subplot(1, 2, 1)
-imshow(content_image, 'Content Image')
-
-plt.subplot(1, 2, 2)
-imshow(style_image, 'Style Image')
 
 #load in VGG19 model for out training
 vgg = tf.keras.applications.VGG19(include_top=False, weights='imagenet')
@@ -99,10 +107,6 @@ def vgg_layers(layer_names):
 
   model = tf.keras.Model([vgg.input], outputs)
   return model
-
-#define the style extractor model
-style_extractor = vgg_layers(style_layers)
-style_outputs = style_extractor(style_image*255)
 
 #function to calculate the gram matrix
 def gram_matrix(input_tensor):
@@ -147,6 +151,21 @@ class StyleContentModel(tf.keras.models.Model):
 
 #create new model and run extractor
 if train_model == 1:
+
+    # display chosen images
+    content_image = load_img(content_path)
+    style_image = load_img(style_path)
+
+    # define the style extractor model
+    style_extractor = vgg_layers(style_layers)
+    style_outputs = style_extractor(style_image * 255)
+
+    plt.subplot(1, 2, 1)
+    imshow(content_image, 'Content Image')
+
+    plt.subplot(1, 2, 2)
+    imshow(style_image, 'Style Image')
+
     extractor = StyleContentModel(style_layers, content_layers)
 
     results = extractor(tf.constant(content_image))
@@ -224,16 +243,47 @@ if train_model == 1:
 elif train_model == 0:
     #because the trained model is so slow, this option uses a faster pretrained model for real time image styling
     #test it on another image.
-    #set up camera here
-    cat_image = load_img('cat.jpg')
-    file_name = 'stylized-image2.png'
+
+    #load style image here
+    style_image = load_img('fractal.jpg')
+    #plt.figure()
+    #imshow(style_image)
+    #plt.show()
+
+    style_on = False
 
     #import module for faster image styling
     import tensorflow_hub as hub
     hub_model = hub.load('https://tfhub.dev/google/magenta/arbitrary-image-stylization-v1-256/2')
-    style_image = hub_model(tf.constant(cat_image), tf.constant(style_image))[0]
-    tensor_to_image(style_image).save(file_name)
-    #show styled image
-    plt.imshow(tensor_to_image(style_image))
-    plt.show()
 
+    # set up camera here
+    #create video object
+    vid = cv.VideoCapture(0)
+
+    while vid.isOpened():
+        #get camera frame
+        ret, frame = vid.read()
+        dim = frame.shape
+        vid_width = dim[0]
+        vid_height = dim[1]
+
+        frame = convert_image(frame)
+        #if setting is enabled convert image, else don't
+        if style_on:
+            #convert image to style
+            my_image = hub_model(tf.constant(frame), tf.constant(style_image))[0]
+            #show styled image
+            new_image = np.array(tensor_to_image(my_image))
+
+        else:
+            new_image = np.array(frame[0])
+
+        #display image
+        cv.imshow('My style', new_image)
+
+        #   wait for escape key to exit app
+        k = cv.waitKey(50) & 0xFF
+        if k == ord('s'):
+            style_on = not style_on
+        if k == 27:
+            break
