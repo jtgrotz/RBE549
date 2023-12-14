1e79c7943ec618f39066310cb8bd5203694823cd
jtgrotz <jordantaylorg007@gmail.com>
2023-12-13 19:46:53 -0500

    zipped up lab 14

---
+++ Lab14/another_nerf.py
@@ -0,0 +1,376 @@
+#incomplete, no need to grade.
+
+import os
+os.environ["KERAS_BACKEND"] = "tensorflow"
+
+import tensorflow as tf
+
+tf.random.set_seed(42)
+
+import keras
+from keras import layers
+
+import glob
+import imageio.v2 as imageio
+import numpy as np
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+import json
+
+#Initialize global variables.
+AUTO = tf.data.AUTOTUNE
+BATCH_SIZE = 5
+NUM_SAMPLES = 32
+POS_ENCODE_DIMS = 16
+EPOCHS = 20
+
+#load in data
+def read_json(jsonPath):
+    with open(jsonPath, "r") as fp:
+        data =  json.load(fp)
+    return data
+
+
+def get_image_c2w(jsonData, datasetPath):
+    # define a list to store the image paths
+    imagePaths = []
+
+    # define a list to store the camera2world matrices
+    c2ws = []
+    # iterate over each frame of the data
+    for frame in jsonData["frames"]:
+        # grab the image file name
+        imagePath = frame["file_path"]
+        imagePath = imagePath.replace(".", datasetPath)
+        imagePaths.append(f"{imagePath}.png")
+        # grab the camera2world matrix
+        c2ws.append(frame["transform_matrix"])
+        # return the image file names and the camera2world matrices
+    return (imagePaths, c2ws)
+
+class GetImages():
+	def __init__(self, imageWidth, imageHeight):
+		# define the image width and height
+		self.imageWidth = imageWidth
+		self.imageHeight = imageHeight
+	def __call__(self, imagePath):
+		# read the image file
+		image = tf.io.read_file(imagePath)
+		# decode the image string
+		image = tf.image.decode_jpeg(image, 3)
+		# convert the image dtype from uint8 to float32
+		image = tf.image.convert_image_dtype(image, dtype=tf.float32)
+		# resize the image to the height and width in config
+		image = tf.image.resize(image, (self.imageWidth, self.imageHeight))
+		image = tf.image.reshape(image, (self.imageWidth, self.imageHeight, 3))
+		# return the image
+		return image
+
+
+
+
+
+# Download the data if it does not already exist.
+url = (
+    "http://cseweb.ucsd.edu/~viscomp/projects/LF/papers/ECCV20/nerf/tiny_nerf_data.npz"
+)
+data = keras.utils.get_file(origin=url)
+
+data = np.load(data)
+images = data["images"]
+im_shape = images.shape
+(num_images, H, W, _) = images.shape
+(poses, focal) = (data["poses"], data["focal"])
+
+# Plot a random image from the dataset for visualization.
+plt.imshow(images[np.random.randint(low=0, high=num_images)])
+plt.show()
+
+#function to encode the ray into fourier features for the position
+def encode_position(x):
+    positions = [x]
+    for i in range(POS_ENCODE_DIMS):
+        for fn in [tf.sin, tf.cos]:
+            positions.append(fn(2.0**i * x))
+    return tf.concat(positions, axis=-1)
+
+#function to get rays from origin point
+def get_rays(height, width, focal, pose):
+    i,j = tf.meshgrid(
+        tf.range(width, dtype=tf.float32),
+        tf.range(height, dtype=tf.float32),
+        indexing="xy",
+    )
+
+    # Normalize the x axis coordinates.
+    transformed_i = (i - width * 0.5) / focal
+
+    # Normalize the y axis coordinates.
+    transformed_j = (j - height * 0.5) / focal
+
+    # Create the direction unit vectors.
+    directions = tf.stack([transformed_i, -transformed_j, -tf.ones_like(i)], axis=-1)
+
+    #get camera matrix
+    camera_matrix = pose[:3,:3]
+    height_width_focal = pose[:3, -1]
+
+    #get origins and directions for the rays
+    transformed_dirs = directions[..., None, :]
+    camera_dirs = transformed_dirs * camera_matrix
+    ray_directions = tf.reduce_sum(camera_dirs, axis=-1)
+    ray_origins = tf.broadcast_to(height_width_focal, tf.shape(ray_directions))
+
+    return(ray_origins, ray_directions)
+
+
+#function for rendering rays
+def render_flat_rays(ray_origins, ray_directions, near, far, num_samples, rand=False):
+    # Compute 3D query points.
+    # Equation: r(t) = o+td -> Building the "t" here.
+    t_vals = tf.linspace(near, far, num_samples)
+    if rand:
+        # Inject uniform noise into sample space to make the sampling
+        # continuous.
+        shape = list(ray_origins.shape[:-1]) + [num_samples]
+        noise = tf.random.uniform(shape=shape) * (far - near) / num_samples
+        t_vals = t_vals + noise
+
+    # Equation: r(t) = o + td -> Building the "r" here.
+    rays = ray_origins[..., None, :] + (
+            ray_directions[..., None, :] * t_vals[..., None]
+    )
+    rays_flat = tf.reshape(rays, [-1, 3])
+    rays_flat = encode_position(rays_flat)
+    return (rays_flat, t_vals)
+
+#function for mapping individual poses to flattened rays
+def map_fn(pose):
+    (ray_origins, ray_directions) = get_rays(height=H, width=W, focal=focal, pose=pose)
+    (rays_flat, t_vals) = render_flat_rays(
+        ray_origins=ray_origins,
+        ray_directions=ray_directions,
+        near=2.0,
+        far=6.0,
+        num_samples=NUM_SAMPLES,
+        rand=True,
+    )
+    return(rays_flat,t_vals)
+
+#split data for training and test
+split_index = int(num_images * 0.8)
+
+train_images = images[:split_index]
+val_images = images[split_index:]
+
+train_poses = poses[:split_index]
+val_poses = poses[split_index:]
+
+#transfer to tf pipeline data structures
+# Make the training pipeline.
+train_img_ds = tf.data.Dataset.from_tensor_slices(train_images)
+train_pose_ds = tf.data.Dataset.from_tensor_slices(train_poses)
+train_ray_ds = train_pose_ds.map(map_fn, num_parallel_calls=AUTO)
+training_ds = tf.data.Dataset.zip((train_img_ds, train_ray_ds))
+train_ds = (
+    training_ds.shuffle(BATCH_SIZE)
+    .batch(BATCH_SIZE, drop_remainder=True, num_parallel_calls=AUTO)
+    .prefetch(AUTO)
+)
+
+
+# Make the validation pipeline.
+val_img_ds = tf.data.Dataset.from_tensor_slices(val_images)
+val_pose_ds = tf.data.Dataset.from_tensor_slices(val_poses)
+val_ray_ds = val_pose_ds.map(map_fn, num_parallel_calls=AUTO)
+validation_ds = tf.data.Dataset.zip((val_img_ds, val_ray_ds))
+val_ds = (
+    validation_ds.shuffle(BATCH_SIZE)
+    .batch(BATCH_SIZE, drop_remainder=True, num_parallel_calls=AUTO)
+    .prefetch(AUTO)
+)
+
+#create nerf model
+def get_nerf_model(num_layers, num_pos):
+    inputs = keras.Input(shape=(num_pos, 2 * 3 * POS_ENCODE_DIMS + 3))
+    x = inputs
+    for i in range(num_layers):
+        x = layers.Dense(units=64, activation="relu")(x)
+        if i % 4 == 0 and i > 0:
+            # Inject residual connection.
+            x = layers.concatenate([x, inputs], axis=-1)
+    outputs = layers.Dense(units=4)(x)
+    return keras.Model(inputs=inputs, outputs=outputs)
+
+#function for rendering rgb image
+def render_rgb_depth(model, rays_flat, t_vals, rand=True, train=True):
+    # Get the predictions from the nerf model and reshape it.
+    if train:
+        predictions = model(rays_flat)
+    else:
+        predictions = model.predict(rays_flat)
+    predictions = tf.reshape(predictions, shape=(BATCH_SIZE, H, W, NUM_SAMPLES, 4))
+
+    # Slice the predictions into rgb and sigma.
+    rgb = tf.sigmoid(predictions[..., :-1])
+    sigma_a = tf.nn.relu(predictions[..., -1])
+
+    # Get the distance of adjacent intervals.
+    delta = t_vals[..., 1:] - t_vals[..., :-1]
+    # delta shape = (num_samples)
+    if rand:
+        delta = tf.concat(
+            [delta, tf.broadcast_to([1e10], shape=(BATCH_SIZE, H, W, 1))], axis=-1
+        )
+        alpha = 1.0 - tf.exp(-sigma_a * delta)
+    else:
+        delta = tf.concat(
+            [delta, tf.broadcast_to([1e10], shape=(BATCH_SIZE, 1))], axis=-1
+        )
+        alpha = 1.0 - tf.exp(-sigma_a * delta[:, None, None, :])
+
+    # Get transmittance.
+    exp_term = 1.0 - alpha
+    epsilon = 1e-10
+    transmittance = tf.math.cumprod(exp_term + epsilon, axis=-1, exclusive=True)
+    weights = alpha * transmittance
+    rgb = tf.reduce_sum(weights[..., None] * rgb, axis=-2)
+
+    if rand:
+        depth_map = tf.reduce_sum(weights * t_vals, axis=-1)
+    else:
+        depth_map = tf.reduce_sum(weights * t_vals[:, None, None], axis=-1)
+    return (rgb, depth_map)
+
+
+#training phase
+class NeRF(keras.Model):
+    def __init__(self, nerf_model):
+        super().__init__()
+        self.nerf_model = nerf_model
+
+    def compile(self, optimizer, loss_fn):
+        super().compile()
+        self.optimizer = optimizer
+        self.loss_fn = loss_fn
+        self.loss_tracker = keras.metrics.Mean(name="loss")
+        self.psnr_metric = keras.metrics.Mean(name="psnr")
+
+    def train_step(self, inputs):
+        (images,rays) = inputs
+        (rays_flat, t_vals) = rays
+
+        with tf.GradientTape() as tape:
+            #get predicitons from model
+            rgb, _ = render_rgb_depth(
+                model=self.nerf_model, rays_flat=rays_flat, t_vals=t_vals, rand=True
+            )
+            loss= self.loss_fn(images, rgb)
+
+        # Get the trainable variables.
+        trainable_variables = self.nerf_model.trainable_variables
+
+        # Get the gradeints of the trainiable variables with respect to the loss.
+        gradients = tape.gradient(loss, trainable_variables)
+
+        # Apply the grads and optimize the model.
+        self.optimizer.apply_gradients(zip(gradients, trainable_variables))
+
+        # Get the PSNR of the reconstructed images and the source images.
+        psnr = tf.image.psnr(images, rgb, max_val=1.0)
+
+        # Compute our own metrics
+        self.loss_tracker.update_state(loss)
+        self.psnr_metric.update_state(psnr)
+        return {"loss": self.loss_tracker.result(), "psnr": self.psnr_metric.result()}
+
+    def test_step(self, inputs):
+        # Get the images and the rays.
+        (images, rays) = inputs
+        (rays_flat, t_vals) = rays
+
+        # Get the predictions from the model.
+        rgb, _ = render_rgb_depth(
+            model=self.nerf_model, rays_flat=rays_flat, t_vals=t_vals, rand=True
+        )
+        loss = self.loss_fn(images, rgb)
+
+        # Get the PSNR of the reconstructed images and the source images.
+        psnr = tf.image.psnr(images, rgb, max_val=1.0)
+
+        # Compute our own metrics
+        self.loss_tracker.update_state(loss)
+        self.psnr_metric.update_state(psnr)
+        return {"loss": self.loss_tracker.result(), "psnr": self.psnr_metric.result()}
+
+    @property
+    def metrics(self):
+        return [self.loss_tracker, self.psnr_metric]
+
+test_imgs, test_rays = next(iter(train_ds))
+test_rays_flat, test_t_vals = test_rays
+
+loss_list = []
+
+#function/class for showing progress of model
+class TrainMonitor(keras.callbacks.Callback):
+    def on_epoch_end(self, epoch, logs=None):
+        loss = logs["loss"]
+        loss_list.append(loss)
+        test_recons_images, depth_maps = render_rgb_depth(
+            model=self.model.nerf_model,
+            rays_flat=test_rays_flat,
+            t_vals=test_t_vals,
+            rand=True,
+            train=False,
+        )
+
+        # Plot the rgb, depth and the loss plot.
+        fig, ax = plt.subplots(nrows=1, ncols=3, figsize=(20, 5))
+        ax[0].imshow(keras.utils.array_to_img(test_recons_images[0]))
+        ax[0].set_title(f"Predicted Image: {epoch:03d}")
+
+        ax[1].imshow(keras.utils.array_to_img(depth_maps[0, ..., None]))
+        ax[1].set_title(f"Depth Map: {epoch:03d}")
+
+        ax[2].plot(loss_list)
+        ax[2].set_xticks(np.arange(0, EPOCHS + 1, 5.0))
+        ax[2].set_title(f"Loss Plot: {epoch:03d}")
+
+        fig.savefig(f"images/{epoch:03d}.png")
+        plt.show()
+        plt.close()
+
+#code for actually running the model
+num_pos = H*W*NUM_SAMPLES
+nerf_model = get_nerf_model(num_layers=8, num_pos=num_pos)
+
+model = NeRF(nerf_model)
+model.compile(
+    optimizer=keras.optimizers.Adam(), loss_fn=keras.losses.MeanSquaredError()
+)
+
+#create directory to save images
+if not os.path.exists("images"):
+    os.makedirs("images")
+
+model.fit(
+    train_ds,
+    validation_data=val_ds,
+    batch_size=BATCH_SIZE,
+    epochs=EPOCHS,
+    callbacks=[TrainMonitor()],
+)
+
+#create git of training model
+def create_gif(path_to_images, name_gif):
+    filenames = glob.glob(path_to_images)
+    filenames = sorted(filenames)
+    images = []
+    for filename in tqdm(filenames):
+        images.append(imageio.imread(filename))
+    kargs = {"duration": 0.25}
+    imageio.mimsave(name_gif, images, "GIF", **kargs)
+
+
+create_gif("images/*.png", "nerf_training.gif")
